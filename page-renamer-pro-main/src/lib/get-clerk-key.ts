58b084ef1b848c743d2a799c1b64614ff31b7632
MHSami08 <masrafihaque398@gmail.com>
2026-08-25 02:21:35 +0600

    Delete page-renamer-pro-main directory

--- page-renamer-pro-main/src/lib/get-clerk-key.ts
+++
@@ -1,7 +0,0 @@
-import { createServerFn } from "@tanstack/react-start";
-
-export const getClerkPublishableKey = createServerFn({ method: "GET" }).handler(async () => {
-  const key = process.env.CLERK_PUBLISHABLE_KEY;
-  if (!key) throw new Error("CLERK_PUBLISHABLE_KEY not configured on the server");
-  return { publishableKey: key };
-});
--- page-renamer-pro-main/src/routes/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { NoteRenamer } from "@/components/note-renamer";
-
-export const Route = createFileRoute("/")({
-  component: NoteRenamer,
-});
